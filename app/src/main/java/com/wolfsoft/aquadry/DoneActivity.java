6cb19a1f0755e23c9f9dbf9dc1abdd3c3c5275df
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-26 16:55:31 +0530

    Initial commit

---
+++ app/src/main/java/com/wolfsoft/aquadry/DoneActivity.java
@@ -0,0 +1,22 @@
+package com.wolfsoft.aquadry;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+public class DoneActivity extends Activity {
+    private TextView next;
+    String latestVersion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+
+    }
+
+
+
+
+}
